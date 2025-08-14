ffa5b101f359e7a12f9ae3c3dd946fb14ded7a42
Abdoulaye <mouhamadou-abdoulaye.gueye@etu.univ-lyon1.fr>
2025-08-14 15:26:30 +0200

    dockerfile update

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN npm run build
 
 FROM nginx:1.27-alpine
 
-COPY --from=build /app/dist/ /usr/share/nginx/html
+COPY --from=build /app/dist/cosf_front /usr/share/nginx/html
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
-export const environment = {
-  production: false,
-  apiUrl: (window as any).ENV?.VITE_APP_API_URL || 'http://localhost:8080/',
+#!/bin/sh
+
+cat <<EOF > /usr/share/nginx/html/env.js
+window.ENV = {
+  VITE_APP_API_URL: "${VITE_APP_API_URL}"
 };
+EOF
+
+exec nginx -g "daemon off;"
